ffab3ea429aa0302543964c16d819e6276ffea94
Denver P <23412100+DrTexxOfficial@users.noreply.github.com>
2019-01-03 15:21:29 +1100

    Windows support added!

--- volux/VolumeAssistant.py
+++ volux/VolumeAssistant.py
@@ -1,5 +1,5 @@
 from _thread import start_new_thread # used to run functions in parallel
-import alsaaudio as al
+import cpaudio
 import time
 
 class VolumeAssistant:
@@ -8,9 +8,7 @@ class VolumeAssistant:
         self.volume = 10 # TODO: change this to poll the actual volume on scroll detection
         self.muted = False # TODO: change this to reflect reality
         self.previous_volume = 0
-        self.device = "Master"
-        self.defaultMixer = "Master"
-        self.mixer = al.Mixer(self.defaultMixer)
+        self.mixer = cpaudio.MixerController()
     def start(self,pollFreq):
         def _startthread(self,pollFreq):
             self.running = True
@@ -28,9 +26,7 @@ class VolumeAssistant:
     def stop(self):
         self.running=False
     def ismuted(self):
-        _muted = self.mixer.getmute()[0]
-        if _muted == 1: return(True)
-        elif _muted == 0: return(False)
+        return(self.mixer.gmute())
     def _get_percentage(self,child,parent):
         percentages = {'x': (child['x']/parent['x'])*100,
                        'y': (child['y']/parent['y'])*100}
--- volux/__main__.py
+++ volux/__main__.py
@@ -1,8 +1,8 @@
-#TODO: fix volume bar not going to zero
+#TODO: fix volume bar not visually going to zero
 #TODO: replace zero volume with mute if supported under Debian
 #TODO: add animation options (e.g. slide in, fade in, etc.)
 #TODO: add numbers to the bar
-#TODO: add support for Windows/Mac (e.g. scroll support)
+#TODO: add support for Mac
 #TODO: allow bar to be attached to different sides
 #TODO: allow customization of bar thickness
 #TODO: make bar opacity adapt to darkness/brightness of background
@@ -19,19 +19,21 @@
 
 # BUILTIN MODULES
 import sys
-import time # used for delays
-import math
-from _thread import start_new_thread # used to run functions in parallel
 import tkinter as Tk
-from subprocess import call
 from os.path import realpath
+
 # SITE PACKAGES #
 from plyer import notification
+
 # LOCAL MODULES
-import volux.temperatures as temps
+#import volux.temperatures as temps
 from volux.dp_datatools import LivePercentage, clamp
 from volux.VolumeAssistant import VolumeAssistant
 from volux.VolumeBar import VolumeBar
+
+import cpaudio
+mixer = cpaudio.MixerController()
+
 ### ---- PREFERENCES ---- ###
 program_title = "Volux"
 program_icon = realpath('icon.png')
@@ -40,24 +42,25 @@ default_mixer_name = "Master"
 default_opacity = 0.5
 outside_zone_opacity = 0.1
 bar_height = 5
+
 ### ---- SETUP STUFF ---- ###
 VolAs = VolumeAssistant() # initialise a Volume Assistant object
 VolBar = VolumeBar()
-coreWatch = temps.CoreWatch(temps.get_cores()) # start watching cores for temperature issues
+#coreWatch = temps.CoreWatch(temps.get_cores()) # start watching cores for temperature issues
 
 ### DEFINE STATES
 class VolumeMode:
     def __init__(self):pass
     def enter(self):
         VolBar.mode = VolBar.modes['volume']
-        VolAs.mixer.setmute(0)
+        mixer.smute(False)
     def vacate(self):
         VolBar.mode = VolBar.modes['unknown']
 class MuteMode:
     def __init__(self): pass
     def enter(self):
         VolBar.mode = VolBar.modes['muted']
-        VolAs.mixer.setmute(1)
+        mixer.smute(True)
     def vacate(self):
         VolBar.mode = VolBar.modes['unknown']
 class BrightnessMode:
@@ -66,28 +69,32 @@ class BrightnessMode:
         VolBar.mode = VolBar.modes['brightness']
     def vacate(self):
         VolBar.mode = VolBar.modes['unknown']
-        if VolAs.ismuted() == True:
+        if mixer.gmute() == True:
             return(MuteMode)
-        elif VolAs.ismuted() == False:
+        elif mixer.gmute() == False:
             return(VolumeMode)
         else: raise TypeError("_ismuted should be a bool value")
-### DEFINE STATE MANAGER
+
+### IMPORT AND INITIALSE STATE MANAGER
 from volux.StateManager import StateManager
-### CREATE A STATE MANAGER
 sm = StateManager(VolumeMode)
 
 ### ---- TKINTER STUFF BEGINS ---- ###
 root = Tk.Tk()
+
 class Window(Tk.Frame):
+
     def __init__(self,master=None):
         Tk.Frame.__init__(self,master)
         self.master = master
         self._init_objects()
         self._init_window()
-        self._open_message()
+        #self._open_message()
+
     def _init_objects(self):
         self.displaySize = VolAs._get_display_size(root) # max size of the percentage bar in pixels
         self.barWidth = LivePercentage(0,self.displaySize['x']) # set width of bar
+
     def _init_window(self):
         m = self.master
         m.title("Please submit an issue to Github if you see this!")
@@ -101,7 +108,8 @@ class Window(Tk.Frame):
             if type(movement) == int: # if movement is an integer
                 #self.barMode = self.barModes['volume']
                 notchMultiplier = 5 # impact of a single scroll notch on percentage
-                newVol = VolAs.volume + movement*notchMultiplier
+                newVol = mixer.gvol() + movement*notchMultiplier
+                mixer.svol(newVol)
                 VolAs.volume = clamp(newVol,0,100)
             else: raise TypeError("Value should be an integer! Not sure what happened!")
             self._update_bar() # update the bar's graphical appearance
@@ -123,6 +131,14 @@ class Window(Tk.Frame):
             elif sm.state == MuteMode:
                 sm.change_state(VolumeMode)
             self._update_bar()
+        def _scroll_delta(event):
+            movement = event.delta/120
+            if movement == 1:
+                _scroll_up(event)
+            elif movement == -1:
+                _scroll_down(event)
+            else:
+                raise ValueError("movement should be 1 or -1")
         def _middle_click(event):
             if sm.state == VolumeMode: # if unmuted
                 sm.change_state(MuteMode) # change to muted
@@ -149,7 +165,7 @@ class Window(Tk.Frame):
             self._update_bar()
             print("brightness mode off!")
         self.bar.pack(fill=Tk.Y,ipadx=5,ipady=5,side=Tk.LEFT)
-        m.bind("<MouseWheel>",_adjust_bar)
+        m.bind("<MouseWheel>",_scroll_delta)
         m.bind("<Button-2>",_middle_click)
         m.bind("<Button-4>",_scroll_up)
         m.bind("<Button-5>",_scroll_down)
@@ -159,23 +175,22 @@ class Window(Tk.Frame):
         m.bind("<Double-Button-3>",self._exit_app)
         barContainer.bind("<Enter>",self._mouse_entered)
         barContainer.bind("<Leave>",self._mouse_left)
+
     def _update_loop(self,ms_per_loop=1000):
         root.lift() # ensure window on top of others
         self._update_bar() # update bar graphics
         self.after(ms_per_loop,self._update_loop) # repeat _update_loop()
+
     def _update_bar(self):
         modeColor = VolBar.mode.color # set background based on mode color
         self.barWidth.setPerc(VolAs.volume) # set the width as a percentage
         newWidth = self.barWidth.getNum() # get a numerical version of the percentage
         self.bar.configure(background=modeColor,width=str(newWidth)) # update the bar with these settings
-    def _update_volume(self):
-        try: self.mixer.setvolume(VolAs.volume)
-        except: call(["amixer","sset",str(VolAs.device),str(VolAs.volume)+"%","-q"])
-    def _update_mute(self):
-        muted = self.mixer.getmute()
-        if muted[0] == True: self.mixer.setmute(0)
-        elif muted[0] == False: self.mixer.setmute(1)
-        else: raise Exception("mixer's .getmute()[0] method should return True or False!")
+
+    def _update_volume(self): mixer.svol(VolAs.volume)
+
+    def _toggle_mute(self): mixer.smute(not mixer.gmute()) # toggle mute state
+
     def _mouse_entered(self,event): root.wm_attributes("-alpha",default_opacity)
     def _mouse_left(self,event): root.wm_attributes("-alpha",outside_zone_opacity)
     def _open_message(self):
@@ -185,13 +200,14 @@ class Window(Tk.Frame):
             app_name=program_title,
             app_icon=program_icon,
             timeout=5)
+
     def _exit_app(self,event):
-        notification.notify(
-            title=program_title,
-            message="{} now closing...".format(program_title),
-            app_name=program_title,
-            app_icon=program_icon,
-            timeout=10)
+#         notification.notify(
+#             title=program_title,
+#             message="{} now closing...".format(program_title),
+#             app_name=program_title,
+#             app_icon=program_icon,
+#             timeout=10)
         exit()
         
 app = Window(root)
