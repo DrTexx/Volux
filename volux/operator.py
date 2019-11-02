7c0b01b77a323ed9d217c58bbeb79262ea77f3c4
DrTexx <23412100+DrTexx@users.noreply.github.com>
2019-11-02 17:14:52 +1100

    formatting with black

--- volux/operator.py
+++ volux/operator.py
@@ -1,28 +1,33 @@
 from .core import VoluxCore
 from .module import VoluxModule
 
+
 class VoluxOperator:
     def __init__(self):
         self.add_module(VoluxCore())
 
-    def add_module(self,module):
+    def add_module(self, module):
 
-        if self.validate_module(module): # if the object passed is a valid module
+        if self.validate_module(module):  # if the object passed is a valid module
 
-            setattr(self,module._module_attr,module)
-            module._loaded() # call modules method for when it's finished being loaded
+            setattr(self, module._module_attr, module)
+            module._loaded()  # call module's method for when it's finished being loaded
 
         else:
 
             raise TypeError("module must be a subclass of VoluxModule")
 
-    def validate_module(self,module):
+    def validate_module(self, module):
 
-        for attrib in ['_module_name','_module_attr']:
+        for attrib in ["_module_name", "_module_attr"]:
 
             if not hasattr(module, attrib):
 
-                print("module must be a subclass of VoluxModule with the [{}] attribute".format(attrib))
+                print(
+                    "module must be a subclass of VoluxModule with the [{}] attribute".format(
+                        attrib
+                    )
+                )
                 return False
 
         return True
