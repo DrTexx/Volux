ef96ad97d0f8684f5217e599bd9a9da8543f7584
DrTexx <23412100+DrTexx@users.noreply.github.com>
2019-11-02 21:50:12 +1100

    GUI implemented for voluxbar

--- voluxbar/__init__.py
+++ voluxbar/__init__.py
@@ -1,9 +1,14 @@
 from volux import VoluxModule
+import tkinter as tk
+from tkinter import ttk
+from time import sleep
 
 class VoluxBar(VoluxModule):
     def __init__(self,*args,**kwargs):
         super().__init__(module_name="Volux Bar",module_attr="bar", module_get=self.get, module_set=self.set)
         self.modes = {}
+        self.root = tk.Tk()
+        self.mainApp = MainApplication(self.root,self,style='Bar.TFrame')
 
     def add_mode(self,name,module):
         self.modes.update({name: module})
@@ -16,3 +21,98 @@ class VoluxBar(VoluxModule):
 
     def set(self, new_val):
         print("WIP!")
+
+    def init_window(self):
+        self.gui_style = ttk.Style()
+        self.gui_style.configure('BarVal.TFrame', background="GREEN")
+        self.gui_style.configure('Bar.TFrame', background="BLACK")
+
+        self.mainApp.pack(side="top", fill=tk.BOTH)
+        self.root.title("title")
+        self.root.attributes("-topmost",True) # force window to stay on top (doesn't work in full screen applications)
+        self.root.overrideredirect(1) # remove frame of window
+        screen_size = (self.root.winfo_screenwidth(), self.root.winfo_screenheight())
+        self.root.geometry("{}x{}+{}+{}".format(
+            1920,
+            50,
+            0,
+            1080-50
+        )) # define the size of the window
+        self.root.wait_visibility(self.root) # required for window transparency
+        self.root.wm_attributes("-alpha",0.2) # make window transparent
+        self.mainApp._update_loop()
+        self.root.mainloop()
+
+
+class MainApplication(ttk.Frame):
+    def __init__(self,parent,VoluxBar_obj,*args,**kwargs):
+        ttk.Frame.__init__(self,parent,*args,**kwargs)
+        self.parent = parent
+        self.VoluxBar_obj = VoluxBar_obj
+
+        self._bar = VolumeBar(self, style='Bar.TFrame')
+        self._bar.pack(fill=tk.Y,side=tk.LEFT)
+
+        self._init_bindings()
+
+    def _update_loop(self,ms_per_loop=1000):
+        self.parent.lift() # ensure window on top of others
+        self._refresh_bar()
+        print("refreshing...")
+        self.after(ms_per_loop,self._update_loop) # repeat _update_loop()
+
+    def _refresh_bar(self):
+        self._bar.barValue.config(width=self.VoluxBar_obj.modes['default'].get())
+        pass
+
+    def _init_bindings(self):
+        # self.barContainer.bind("<Enter>",self._mouse_entered)
+        # self.barContainer.bind("<Leave>",self._mouse_left)
+        self.parent.bind("<MouseWheel>",self._scroll_delta)
+        # self.master.bind("<Button-2>",self._middle_click)
+        self.parent.bind("<Button-4>",self._scroll_up)
+        self.parent.bind("<Button-5>",self._scroll_down)
+        # self.master.bind("<Button-3>",self._right_click)
+        # self.master.bind("<Control-Button-4>",self._brightness_up)
+        # self.master.bind("<Control-Button-5>",self._brightness_down)
+        self.master.bind("<Double-Button-3>",self._exit_app)
+
+    def _scroll_delta(self, event):
+
+        movement = event.delta/120
+
+        if movement == 1:
+            self._scroll_up(event)
+
+        elif movement == -1:
+            self._scroll_down(event)
+
+        else:
+            raise ValueError("movement should be 1 or -1")
+
+    def _scroll_up(self, event):
+
+        print("up!")
+        val = self.VoluxBar_obj.modes['default'].get()
+        self.VoluxBar_obj.modes['default'].set(val+20)
+        self._refresh_bar()
+
+    def _scroll_down(self, event):
+
+        print("down!")
+        val = self.VoluxBar_obj.modes['default'].get()
+        self.VoluxBar_obj.modes['default'].set(val-20)
+        self._refresh_bar()
+
+    def _exit_app(self, event):
+
+        exit()
+
+
+class VolumeBar(ttk.Frame):
+    def __init__(self,parent,*args,**kwargs):
+        ttk.Frame.__init__(self,parent,*args,**kwargs)
+        self.parent = parent
+
+        self.barValue = ttk.Frame(self,width=1920*0.5,height=100,style="BarVal.TFrame")
+        self.barValue.pack()
