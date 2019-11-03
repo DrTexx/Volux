60bfa3dac5b0020180b159da0d65b521fff4d0c7
DrTexx <23412100+DrTexx@users.noreply.github.com>
2019-11-03 13:16:50 +1100

    remove main test

--- test_volux_main.py
+++
@@ -1,6 +0,0 @@
-import pytest
-import volux
-
-class Test_volux_main:
-    def test_main(self):
-        from volux import __main__
--- volux/__main__.py
+++ volux/__main__.py
@@ -4,18 +4,18 @@ from voluxdemomodule import VoluxDemoModule
 from voluxvolume import VoluxVolume
 from voluxlight import VoluxLight
 
-# create Volux Operator object (hub for communication between modules)
-vlx = volux.VoluxOperator()
-
-# load Volux Bar module
-vlx.add_module(VoluxBar())
-vlx.add_module(VoluxDemoModule())
-vlx.add_module(VoluxVolume())
-vlx.add_module(VoluxLight("Bob's Lamp"))
-
-vlx.bar.add_mode("default",vlx.volume)
-vlx.bar.add_mode("light",vlx.light)
-
 if __name__ == '__main__':
+
+    # create Volux Operator object (hub for communication between modules)
+    vlx = volux.VoluxOperator()
+
+    # load Volux Bar module
+    vlx.add_module(VoluxBar())
+    vlx.add_module(VoluxDemoModule())
+    vlx.add_module(VoluxVolume())
+    vlx.add_module(VoluxLight("Bob's Lamp"))
+
+    vlx.bar.add_mode("default",vlx.volume)
+    vlx.bar.add_mode("light",vlx.light)
 
     vlx.bar.init_window()
