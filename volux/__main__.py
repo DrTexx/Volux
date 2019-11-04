3a0d462fe4411331b6f3c9cd57eebfe5926d12ef
DrTexx <23412100+DrTexx@users.noreply.github.com>
2019-11-04 18:29:58 +1100

    overhaul system argument handling

--- volux/__main__.py
+++ volux/__main__.py
@@ -1,56 +1,120 @@
-# import argparse
-#
-# parser = argparse.ArgumentParser(description='A platform for controlling your media/entertainment workflows.')
-#
-# parser.add_argument(
-#     'command'
-# )
-#
-# parser.add_argument(
-#     'integers',
-#     metavar='N',
-#     type=int,
-#     nargs='+',
-#     help='an integer for the accumulator'
-# )
-#
-# parser.add_argument(
-#     '--sum',
-#     dest='accumulate',
-#     action='store_const',
-#     const=sum,
-#     default=max,
-#     help='sum the integers (default: find the max)'
-# )
-# args = parser.parse_args()
-#
-# print(args.accumulate(args.integers))
+#!/usr/bin/env python3 -e
 
-import sys
+import argparse
+import colorama
 
-if len(sys.argv) > 1:
+def get_python_module_items(module):
 
-    if sys.argv[1] == 'demo':
+    return [getattr(module,item_name) for item_name in dir(module)]
 
-        if len(sys.argv) > 2:
+def filter_by_attr_value(module_items,attribute,attribute_value):
 
-            if sys.argv[2] == 'bar':
+    valid_items = []
 
-                from volux.demos import demo_volbar
-                demo_volbar.run_demo()
+    for item in module_items:
 
-            else:
+        if hasattr(item,attribute):
 
-                raise Exception("'{}' is not a valid demo".format(sys.argv[2]))
+            if getattr(item,attribute) == attribute_value:
+
+                valid_items.append(item)
+
+    return(valid_items)
+
+def filter_by_superclass(items,superclass):
+    """return all objects which have inherited a particular superclass"""
+
+    return filter_by_attr_value(items, 'superclass', superclass)
+
+def gen_demo_dict(demo_list):
+
+    demo_dict = {demo._alias: demo for demo in demo_list}
+    return demo_dict
+
+def main():
+
+    # Initialize the parser
+    parser = argparse.ArgumentParser(description="High-level media/entertainment workflow automation platform")
+
+    # Add the positional parameter
+    parser.add_argument('-l','--list', action="store", choices=['demos','modules'], help="return a list of avaiable items", default=None)
+    parser.add_argument('-d','--demo', action="store", help="run the specified demo")
+
+    # Parse the arguments
+    arguments = parser.parse_args()
+
+    # Finally print the passed string
+    if arguments.list == 'demos':
+
+        from volux import demos, VoluxDemo
+
+        items = get_python_module_items(demos) # for each item in the demos module
+        demos_collected = filter_by_superclass(items,VoluxDemo) # filter out items not inherited from VoluxDemo class
+        demo_aliases = [demo._alias for demo in demos_collected]
+        print("available demos:",demo_aliases)
+
+    elif arguments.list == 'modules':
+
+        raise NotImplementedError()
+
+    elif not arguments.demo == None:
+
+        from volux import demos, VoluxDemo
+
+        items = get_python_module_items(demos) # for each item in the demos module
+        demos_collected = filter_by_superclass(items,VoluxDemo) # filter out items not inherited from VoluxDemo class
+        demo_dict = gen_demo_dict(demos_collected)
+
+        if arguments.demo in demo_dict:
+
+            demo_dict[arguments.demo].run_demo()
 
         else:
 
-            raise Exception("Please specify a demo to run")
+            print("'{}' is not a valid demo".format(arguments.demo))
 
     else:
 
-        raise Exception("'{}' is not a valid argument".format(sys.argv[1]))
+        print("{}error: not enough arguments{}".format(colorama.Fore.RED,colorama.Style.RESET_ALL))
+        print("{}tip: type 'volux --help' for a list of commands{}".format(colorama.Fore.YELLOW,colorama.Style.RESET_ALL))
 
-else:
+    # for i in range(0, arguments.repeat):
+    #
+    #     print(arguments.printme)
 
-    print("ERROR: too few arguments")
+if __name__ == '__main__':
+
+    main()
+
+
+# def main():
+#
+#     if len(sys.argv) > 1:
+#
+#         if sys.argv[1] == 'demo':
+#
+#             if len(sys.argv) > 2:
+#
+#                 if sys.argv[2] == 'bar':
+#
+#                     from volux.demos import demo_volbar
+#                     demo_volbar.run_demo()
+#
+#                 else:
+#
+#                     raise Exception("'{}' is not a valid demo".format(sys.argv[2]))
+#
+#             else:
+#
+#                 raise Exception("Please specify a demo to run")
+#
+#         else:
+#
+#             raise Exception("'{}' is not a valid argument".format(sys.argv[1]))
+#
+#     else:
+#
+#         print("ERROR: too few arguments")
+#
+# if __name__ == '__main__':
+#     main()
