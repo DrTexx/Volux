d0a834a735818086cbf6bb7251637e8c64bdbdf5
DrTexx <23412100+DrTexx@users.noreply.github.com>
2019-11-04 20:11:54 +1100

    update tests

--- test_operator.py
+++ test_operator.py
@@ -1,27 +1,27 @@
 import pytest
 import volux
-from voluxdemomodule import VoluxDemoModule
+from voluxcliprint import VoluxCliPrint
 
 # create Volux Operator object (hub for communication between modules)
 vlx = volux.VoluxOperator()
-demo_module = VoluxDemoModule()
+cli_module   = VoluxCliPrint()
 
 class Test_operator:
     def test_add_module(self):
-        vlx.add_module(demo_module)
-        assert demo_module in vlx.modules
+        vlx.add_module(cli_module)
+        assert cli_module in vlx.modules
 
     def test_add_bad_module(self):
         with pytest.raises(TypeError):
             vlx.add_module(vlx)
 
     def test_remove_module(self):
-        vlx.remove_module(demo_module)
-        assert not (demo_module in vlx.modules)
+        vlx.remove_module(cli_module)
+        assert not (cli_module in vlx.modules)
 
     def test_remove_missing_module(self):
         with pytest.raises(AttributeError):
-            vlx.remove_module(demo_module)
+            vlx.remove_module(cli_module)
 
     def test_validate_module(self):
         assert vlx.validate_module(vlx) == False
