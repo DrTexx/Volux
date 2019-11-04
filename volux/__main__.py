09e38fcfea845d2b4cfd9c39120d6e017835fd97
DrTexx <23412100+DrTexx@users.noreply.github.com>
2019-11-05 01:26:08 +1100

    refactor __main__ and VoluxCore module

--- volux/__main__.py
+++ volux/__main__.py
@@ -4,34 +4,6 @@ import argparse
 import colorama
 colorama.init()
 
-def get_python_module_items(module):
-
-    return [getattr(module,item_name) for item_name in dir(module)]
-
-def filter_by_attr_value(module_items,attribute,attribute_value):
-
-    valid_items = []
-
-    for item in module_items:
-
-        if hasattr(item,attribute):
-
-            if getattr(item,attribute) == attribute_value:
-
-                valid_items.append(item)
-
-    return(valid_items)
-
-def filter_by_superclass(items,superclass):
-    """return all objects which have inherited a particular superclass"""
-
-    return filter_by_attr_value(items, 'superclass', superclass)
-
-def gen_demo_dict(demo_list):
-
-    demo_dict = {demo._alias: demo for demo in demo_list}
-    return demo_dict
-
 def main():
 
     # Initialize the parser
@@ -47,10 +19,12 @@ def main():
     # Finally print the passed string
     if arguments.list == 'demos':
 
-        from volux import demos, VoluxDemo
+        from volux import demos, VoluxDemo, VoluxCore
 
-        items = get_python_module_items(demos) # for each item in the demos module
-        demos_collected = filter_by_superclass(items,VoluxDemo) # filter out items not inherited from VoluxDemo class
+        _core = VoluxCore()
+
+        items = _core.get_python_module_items(demos) # for each item in the demos module
+        demos_collected = _core.filter_by_superclass(items,VoluxDemo) # filter out items not inherited from VoluxDemo class
         demo_aliases = [demo._alias for demo in demos_collected]
         print("available demos:",demo_aliases)
 
@@ -60,11 +34,13 @@ def main():
 
     elif not arguments.demo == None:
 
-        from volux import demos, VoluxDemo
+        from volux import demos, VoluxDemo, VoluxCore
 
-        items = get_python_module_items(demos) # for each item in the demos module
-        demos_collected = filter_by_superclass(items,VoluxDemo) # filter out items not inherited from VoluxDemo class
-        demo_dict = gen_demo_dict(demos_collected)
+        _core = VoluxCore()
+
+        items = _core.get_python_module_items(demos) # for each item in the demos module
+        demos_collected = _core.filter_by_superclass(items,VoluxDemo) # filter out items not inherited from VoluxDemo class
+        demo_dict = _core.gen_demo_dict(demos_collected)
 
         if arguments.demo in demo_dict:
 
@@ -87,36 +63,3 @@ def main():
 if __name__ == '__main__':
 
     main()
-
-
-# def main():
-#
-#     if len(sys.argv) > 1:
-#
-#         if sys.argv[1] == 'demo':
-#
-#             if len(sys.argv) > 2:
-#
-#                 if sys.argv[2] == 'bar':
-#
-#                     from volux.demos import demo_volbar
-#                     demo_volbar.run_demo()
-#
-#                 else:
-#
-#                     raise Exception("'{}' is not a valid demo".format(sys.argv[2]))
-#
-#             else:
-#
-#                 raise Exception("Please specify a demo to run")
-#
-#         else:
-#
-#             raise Exception("'{}' is not a valid argument".format(sys.argv[1]))
-#
-#     else:
-#
-#         print("ERROR: too few arguments")
-#
-# if __name__ == '__main__':
-#     main()
--- volux/core.py
+++ volux/core.py
@@ -3,3 +3,31 @@ from .module import VoluxModule
 class VoluxCore(VoluxModule):
     def __init__(self,*args,**kwargs):
         super().__init__(module_name="Volux Core",module_attr="core",module_get=None,module_set=None)
+
+    def get_python_module_items(self,module):
+
+        return [getattr(module,item_name) for item_name in dir(module)]
+
+    def filter_by_attr_value(self,module_items,attribute,attribute_value):
+
+        valid_items = []
+
+        for item in module_items:
+
+            if hasattr(item,attribute):
+
+                if getattr(item,attribute) == attribute_value:
+
+                    valid_items.append(item)
+
+        return(valid_items)
+
+    def filter_by_superclass(self,items,superclass):
+        """return all objects which have inherited a particular superclass"""
+
+        return self.filter_by_attr_value(items, 'superclass', superclass)
+
+    def gen_demo_dict(self,demo_list):
+
+        demo_dict = {demo._alias: demo for demo in demo_list}
+        return demo_dict
