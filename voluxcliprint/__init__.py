35b00aec73de2874c60e02337e762597cf740338
DrTexx <23412100+DrTexx@users.noreply.github.com>
2019-11-04 20:18:55 +1100

    simplify VoluxCliPrint module

--- voluxcliprint/__init__.py
+++ voluxcliprint/__init__.py
@@ -1,33 +1,25 @@
 from volux import VoluxModule
 
+
 class VoluxCliPrint(VoluxModule):
-    def __init__(self,min_val=0,max_val=100,*args,**kwargs):
+    def __init__(self, min_val=0, max_val=100, *args, **kwargs):
         super().__init__(
             module_name="Volux CLI Print",
             module_attr="demo",
             module_get=self.get,
-            module_set=self.set
+            module_set=self.set,
         )
         self.demo_val = 0
         self.min_val = min_val
         self.max_val = max_val
 
     def get(self):
+
         return self.demo_val
 
-    def set(self, new_val):
+    def set(self, new_val, verbose=True):
 
-        clamped_to = None
+        self.demo_val = new_val
 
-        if (new_val < self.min_val):
-            self.demo_val = self.min_val
-            clamped_to = "min"
-
-        elif (new_val > self.max_val):
-            self.demo_val = self.max_val
-            clamped_to = "max"
-
-        else:
-            self.demo_val = new_val
-
-        print("demo_val: {} (clamped by [{}])".format(self.demo_val,clamped_to))
+        if verbose == True:
+            print("demo_val: {}".format(self.demo_val))
