5cae5ec90e9c8616e3b96df076863dedfa693765
DrTexx <23412100+DrTexx@users.noreply.github.com>
2019-11-05 01:50:34 +1100

    remove test

--- test_volux_demos.py
+++ test_volux_demos.py
@@ -4,7 +4,3 @@ import volux
 class Test_volux_demos:
     def test_import_demos(self):
         from volux import demos
-
-    def test_messed_up_path(self):
-        with pytest.raises(ModuleNotFoundError):
-            import demos
