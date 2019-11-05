9b8db6c52598145f2e2a39adf9b6df2b9849a0a6
DrTexx <23412100+DrTexx@users.noreply.github.com>
2019-11-06 00:42:13 +1100

    add core test

--- test_volux_core.py
+++ test_volux_core.py
@@ -6,6 +6,10 @@ vlx = volux.VoluxOperator()
 module_items = []
 demos_collected = []
 
+class DecoyClass:
+    def __init__(self):
+        self.superclass = "this string is certainly not a class"
+
 class Test_operator:
     def test_correct_type(self):
 
@@ -20,6 +24,7 @@ class Test_operator:
     def test_filter_by_superclass(self):
 
         global module_items
+        module_items.append(DecoyClass())
         global demos_collected
         demos_collected = vlx.core.filter_by_superclass(module_items,volux.VoluxDemo)
 
