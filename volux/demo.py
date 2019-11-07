588174d98d9054b2394e19506cba36d4f2f9b09c
DrTexx <23412100+DrTexx@users.noreply.github.com>
2019-11-07 22:50:47 +1100

    remove wip str rep

--- volux/demo.py
+++ volux/demo.py
@@ -8,12 +8,12 @@ class VoluxDemo:
     def run(self):
         self._method()
 
-    def __str__(self):
-        header = "[DEMO:{name}]----".format(name=self._alias)
-        divider = "-"*len(header)
-        body = "Name: {name}\nAlias: {alias}\nRequirements: {requirements}".format(name=self._name,requirements=self._requirements, alias=self._alias)
-
-        return("{header}\n{body}\n{divider}".format(header=header,divider=divider,body=body))
+    # def __str__(self):
+    #     header = "[DEMO:{name}]----".format(name=self._alias)
+    #     divider = "-"*len(header)
+    #     body = "Name: {name}\nAlias: {alias}\nRequirements: {requirements}".format(name=self._name,requirements=self._requirements, alias=self._alias)
+    #
+    #     return("{header}\n{body}\n{divider}".format(header=header,divider=divider,body=body))
 
     def __repr__(self):
         return "<VoluxDemo '{}'>".format(self._alias)
