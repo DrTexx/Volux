282e089fd4b6bb87284f221a571a6222487ac5fa
DrTexx <23412100+DrTexx@users.noreply.github.com>
2019-11-07 22:46:39 +1100

    fix for cliprint module

--- voluxcliprint/__init__.py
+++ voluxcliprint/__init__.py
@@ -2,24 +2,22 @@ from volux import VoluxModule
 
 
 class VoluxCliPrint(VoluxModule):
-    def __init__(self, min_val=0, max_val=100, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         super().__init__(
             module_name="Volux CLI Print",
             module_attr="cli",
             module_get=self.get,
             module_set=self.set,
-            shared_modules=[]
+            shared_modules=[],
+            pollrate=None
         )
-        self.demo_val = 0
-        self.min_val = min_val
-        self.max_val = max_val
+        self.cli_val = 0
 
     def get(self):
 
-        return self.demo_val
+        return self.cli_val
 
     def set(self, new_val):
 
-        self.demo_val = new_val
-
-        print("demo_val: {}".format(self.demo_val))
+        self.cli_val = new_val
+        print("VoluxCliPrint new value: {}".format(self.cli_val))
