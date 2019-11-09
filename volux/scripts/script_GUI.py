3cd0cb032dfd136baece7199619d6358cf42665a
DrTexx <23412100+DrTexx@users.noreply.github.com>
2019-11-10 03:35:30 +1100

    improvements to GUI script

--- volux/scripts/script_GUI.py
+++ volux/scripts/script_GUI.py
@@ -12,49 +12,53 @@ def launch_gui():
     from voluxaudio import VoluxAudio
     from voluxgui import VoluxGui
 
-    visHz = 240
-
-    vlx = VoluxOperator()
-    vlx.add_module(VoluxCliPrint())
-    vlx.add_module(VoluxLight(instance_label="strip",init_mode="device",init_mode_args={'label': 'Strip'}))
-    vlx.add_module(VoluxLight(instance_label="lamp",init_mode="device",init_mode_args={'label': 'Demo Bulb'}))
-    vlx.add_module(VoluxAudio(sensitivity=8))
-    # vlx.add_module(VoluxLight(init_mode="device",init_mode_args={'label': 'Demo Bulb'}))  # fix: overwrites the first instance, needs addressing
-
-    gui_shared_modules = [vlx.light_lamp,vlx.light_strip,vlx.cli,vlx.audio]
-
+    # inital values
     vis_added = False
 
-    try:
+    # create instance of VoluxOperator
+    vlx = VoluxOperator()
+    vlx.add_module(VoluxCliPrint())  # add VoluxCliPrint module
+    vlx.add_module(VoluxAudio(sensitivity=8))  # add VoluxAudio module
 
-        # raise Exception("A BIG ERROR")
+    gui_shared_modules = [vlx.cli,vlx.audio]
+
+    # try adding voluxlight module
+    try:
+        vlx.add_module(VoluxLight(instance_label="office",init_mode="group",init_mode_args={'group_label': 'Office'}))
+        gui_shared_modules.append(vlx.light_office)
+    except Exception as err:
+        print("{}WARNING: couldn't add light module/s... reason: {}{}".format(colorama.Fore.YELLOW,err,colorama.Style.RESET_ALL))
+
+    # try adding voluxlightvisualiser module
+    try:
         from voluxlightvisualiser import VoluxLightVisualiser, INTENSE_MODE
         vlx.add_module(VoluxLightVisualiser(mode=INTENSE_MODE,packetHz=240,hueHz=240,hue_cycle_duration=5))
         gui_shared_modules.append(vlx.vis)
         vis_added = True
-
     except Exception as err:
-
         print("{}WARNING: couldn't add visualiser module... reason: {}{}".format(colorama.Fore.YELLOW,err,colorama.Style.RESET_ALL))
 
     vlx.add_module(VoluxGui(shared_modules=gui_shared_modules))
 
     if vis_added == True:
+        visHz = 240
         def vis_func(loop_hz):
             while True:
-                while vlx.gui.mainApp.vis_frame.vis_on.get() == True:
-                    vlx.vis.set(vlx.audio.get())
-                    vis_get = vlx.vis.get_from_gui()
-                    vlx.light_strip.set_color(vis_get)
-                    vlx.light_lamp.set_color(vis_get)
-                    sleep(1/loop_hz)
+                if vlx.gui.mainApp.vis_frame.vis_on.get() == True:
+                    vlx.vis.start()
+                    while vlx.gui.mainApp.vis_frame.vis_on.get() == True:
+                        vlx.vis.set(vlx.audio.get())
+                        vis_get = vlx.vis.get_from_gui()
+                        vlx.light_office.set_color(vis_get)
+                        sleep(1/loop_hz)
+                    vlx.vis.stop()
 
-                    # print("START VISUALISER")
-                    # vlx.vis.start()
-                    # while vlx.vis._enabled == True:
-                    #     vlx.vis.set(vlx.audio.get())
-                    #     vlx.light.set_color(vlx.vis.get())
-                    #     sleep(1/loop_hz)
+                        # print("START VISUALISER")
+                        # vlx.vis.start()
+                        # while vlx.vis._enabled == True:
+                        #     vlx.vis.set(vlx.audio.get())
+                        #     vlx.light.set_color(vlx.vis.get())
+                        #     sleep(1/loop_hz)
                 sleep(1)
 
         _vis_func_thread = threading.Thread(target=vis_func,args=(visHz,))
