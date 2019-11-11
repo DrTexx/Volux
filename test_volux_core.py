dec043b115bb64df539c34b3fbac14f4c3a01f3c
DrTexx <23412100+DrTexx@users.noreply.github.com>
2019-11-11 11:07:02 +1100

    added/improved tests for volux core

--- test_volux_core.py
+++ test_volux_core.py
@@ -20,6 +20,10 @@ class Test_operator:
         from volux import demos
         global module_items
         module_items = vlx.core.get_python_module_items(demos)
+        # print("MODULE ITEMS",module_items)
+        raw_module_items = [getattr(demos,item_name) for item_name in dir(demos)]
+        for module_item in module_items:
+            assert module_item in raw_module_items
 
     def test_filter_by_superclass(self):
 
@@ -28,8 +32,28 @@ class Test_operator:
         global demos_collected
         demos_collected = vlx.core.filter_by_superclass(module_items,volux.VoluxDemo)
 
+        for demo_x in demos_collected:
+            assert demo_x.superclass == volux.VoluxDemo
+
     def test_gen_demo_dict(self):
 
         global demo_dict
         global demos_collected
         demo_dict = vlx.core.gen_demo_dict(demos_collected)
+
+        for demo_name in demo_dict:
+            assert demo_dict[demo_name].superclass == volux.VoluxDemo
+
+    def test_get_demos(self):
+
+        for demo_x in vlx.core.get_demos():
+            assert demo_x.superclass == volux.VoluxDemo
+
+    def test_get_script_names(self):
+
+        script_names = vlx.core.get_script_names()
+        dir_of_scripts = dir(volux.scripts)
+
+        for script_name in script_names:
+            assert script_name in dir_of_scripts
+            assert "script_" in script_name
