9b5097dc19782e086b199883da005e1fde77403a
DrTexx <23412100+DrTexxOfficial@users.noreply.github.com>
2019-11-15 00:05:41 +1100

    created a demo simple bar visualiser workflow

---
+++ demo_audio_light_sync.py
@@ -0,0 +1,12 @@
+import volux  # import essentials
+from voluxaudio import VoluxAudio  # module to get amplitude of desktop audio
+from voluxcliprint import VoluxCliPrint  # module to print values to CLI
+from voluxgui import VoluxGui  # module to generate GUI
+vlx = volux.VoluxOperator()  # instantiate an operator for managing connections
+vlx.add_module(VoluxAudio())
+vlx.add_module(VoluxCliPrint())
+vlx.add_module(VoluxGui())
+vlx.add_connection(volux.VoluxConnection(vlx.audio,vlx.gui,120))  # change GUI bar to match audio amplitude
+# vlx.add_connection(volux.VoluxConnection(vlx.audio,vlx.cli,120))  # print audio amplitude to terminal
+vlx.start_sync()
+vlx.gui.init_window()
