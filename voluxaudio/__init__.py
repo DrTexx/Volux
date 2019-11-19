059d19e037f20f5630cd5f05f1b5c9e5ae263a6c
DrTexx <23412100+DrTexx@users.noreply.github.com>
2019-11-19 16:40:46 +1100

    [VoluxAudio] remove lifxtools requirement

--- voluxaudio/__init__.py
+++ voluxaudio/__init__.py
@@ -2,7 +2,6 @@ from volux import VoluxModule
 import numpy as np
 import pyaudio
 import colorama
-from voluxaudio import lifxtools
 from time import sleep
 from .hsv2ansi import *
 
@@ -54,7 +53,7 @@ class VoluxAudio(VoluxModule):
             shared_modules=shared_modules,
             pollrate=pollrate
         )
-        self.pa = pyaudio.PyAudio()
+        self.pa = pyaudio.PyAudio()  # note: this is what causes all the spam when creating module
         self.stream = self._open_stream()
         self.sensitivity = 9
 
@@ -90,51 +89,6 @@ class VoluxAudio(VoluxModule):
         adj_audio_norm = raw_audio_norm / 2 ** self.sensitivity
         cla_audio_norm = clamp(adj_audio_norm, 0, 1)  # normalized audio returning a value between 0 to 1
         return cla_audio_norm * 100 # 0 .. 100
-
-    def run_it(self):
-
-        # ---- settings ----
-
-        update_Hz = 240      # 120 is default, low-spec PC's need lower Hz, lights can't
-                            # handle Hz too high and crash intermitently
-                            # note: tilechains take a lot more processing power to do
-                            # realtime music-vis, 60 Hz pretty safe for 1 tilechain
-                            # if n in ([n]Hz Δ) is any lower than 0, your computer is
-                            # struggling to calculate colors quick enough,
-                            # try reducing update_Hz for more accurate music visualisation
-                            # note: lifx recommends only sending packets at 50Hz (20ms
-                            # per packet) but I'll let you decide if it's worth it
-        fade = 20 # in milliseconds
-        min_brightness = 1 # default value - 1 (if this is 0 responsiveness might be impacted negatively)
-                                   # 65535*0.5 if you want something more bearable on more intense modes
-        max_brightness = 65535*1 # default value - 65535
-        min_saturation = 65535*0 # default value - 0
-        max_saturation = 65535*1 # default value - 65535
-        color_temp = 6500 # default: 6500, gnome-NightLight: 4000
-        amp_sensitivity = 9 # default: 9 (lower = higher sensitivity, vice versa)
-        hue_cycle_duration = 5
-        verbose = False
-
-        if (update_Hz < 30):
-
-            print(colorama.Back.RED + colorama.Fore.WHITE + "update_Hz is < 30, enabling automatic fade calculation" + colorama.Style.RESET_ALL)
-            sleep(1)
-
-            if (update_Hz >= 1):
-
-                fade = 1000/update_Hz # automatically determine transition duration based on the time between color packets sent to the bulbs (especially makes slower Hz look better)
-
-            else:
-
-                raise ValueError("update_Hz must be greater than or equal to 1!")
-
-        # ---- script ----
-
-        mlifx = lifxtools.ManagedLifx(lifxtools.return_interface(None),verbose=verbose)
-
-        self.lal = lifxtools.LightShow(mlifx,pyaudio,update_Hz,fade,min_brightness=min_brightness,max_brightness=max_brightness,min_saturation=min_saturation,max_saturation=max_saturation,color_temp=color_temp,hue_cycle_duration=hue_cycle_duration,amp_sensitivity=amp_sensitivity)
-
-        self.lal.start()
 
     def stop(self):
 
