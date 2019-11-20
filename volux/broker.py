bce97e94b57fc236b456500f04185254dc09b392
DrTexx <23412100+DrTexx@users.noreply.github.com>
2019-11-20 11:33:09 +1100

    [broker] format with black

--- volux/broker.py
+++ volux/broker.py
@@ -1,30 +1,53 @@
 from .requests import *
 
+
 class VoluxBroker:
-    def __init__(self,operator):
+    def __init__(self, operator):
         self.operator = operator
 
-    def process_request(self,request, verbose=True):
+    def process_request(self, request, verbose=True):
 
-        if issubclass(type(request),VoluxBrokerRequest) == True:
+        if issubclass(type(request), VoluxBrokerRequest) == True:
 
-            if verbose == True: print("request from {} is a valid subclass...".format(request.module._module_name))
+            if verbose == True:
+
+                print(
+                    "request from {} is a valid subclass...".format(
+                        request.module._module_name
+                    )
+                )
 
             mUUID = request.module.UUID
 
-            if verbose == True: print("UUID of requesting module:",mUUID)
+            if verbose == True:
+
+                print("UUID of requesting module:", mUUID)
 
             if type(request) in request.module.req_permissions:
 
-                if verbose == True: print("{} claims it's allowed to {}".format(request.module._module_name,request.req_string))
+                if verbose == True:
+
+                    print(
+                        "{} claims it's allowed to {}".format(
+                            request.module._module_name, request.req_string
+                        )
+                    )
 
                 if type(request) in self.operator.permissions[mUUID]:
 
-                    if verbose == True: print("Operator verified module's permissions")
+                    if verbose == True:
+
+                        print("Operator verified module's permissions")
 
                     req_type = type(request)
 
-                    if verbose == True: print("{} is requesting to '{}'...".format(request.module._module_name,request.req_string))
+                    if verbose == True:
+
+                        print(
+                            "{} is requesting to '{}'...".format(
+                                request.module._module_name, request.req_string
+                            )
+                        )
 
                     if req_type == RequestAddConnection:
 
@@ -52,15 +75,27 @@ class VoluxBroker:
 
                     else:
 
-                        raise TypeError("volux broker recieved an invalid request type: {}".format(type(request)))
+                        raise TypeError(
+                            "volux broker recieved an invalid request type: {}".format(
+                                type(request)
+                            )
+                        )
 
                 else:
 
-                    raise PermissionError("operator could not verify module '{}' had valid permissions".format(request.module._module_name))
+                    raise PermissionError(
+                        "operator could not verify module '{}' had valid permissions".format(
+                            request.module._module_name
+                        )
+                    )
 
             else:
 
-                raise PermissionError("module {} doesn't claim it's allowed to {} ({})".format(request.module._module_name,request.req_string,type(request)))
+                raise PermissionError(
+                    "module {} doesn't claim it's allowed to {} ({})".format(
+                        request.module._module_name, request.req_string, type(request)
+                    )
+                )
 
         else:
 
