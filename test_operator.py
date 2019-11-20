f19ee45cd5d76babd4be19f8697636e004f8ebdc
DrTexx <23412100+DrTexx@users.noreply.github.com>
2019-11-20 11:23:51 +1100

    improve operator tests

--- test_operator.py
+++ test_operator.py
@@ -7,6 +7,7 @@ vlx = volux.VoluxOperator()
 cli_module = VoluxCliPrint()
 cli_UUID = None
 
+
 class Test_operator:
     def test_add_module(self):
         global cli_UUID
@@ -22,7 +23,6 @@ class Test_operator:
             vlx.add_module(vlx)
 
     def test_remove_module(self):
-        global cli_UUID
         assert cli_UUID in vlx.modules
         vlx.remove_module(cli_module)
         assert not (cli_UUID in vlx.modules)
@@ -30,6 +30,7 @@ class Test_operator:
     def test_remove_missing_module(self):
         with pytest.raises(AttributeError):
             vlx.remove_module(cli_module)
+        vlx.add_module(cli_module)
 
     def test_validate_module(self):
         assert vlx.validate_module(vlx) == False
@@ -37,6 +38,16 @@ class Test_operator:
 
     def test_get_modules(self):
         assert vlx.get_modules() == vlx.modules
+
+    def test_add_connection(self):
+        vlx.add_connection(volux.VoluxConnection(vlx.cli, vlx.core, 30))
+
+    def test_start_sync(self):
+        vlx.start_sync()
+
+    def test_stop_sync(self):
+        vlx.stop_sync()
+
 
 if __name__ == "__main__":
     test_op = Test_operator()
