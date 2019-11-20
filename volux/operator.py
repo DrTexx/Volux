676250704f7a06ce1375416cfc57c0898bf136fb
DrTexx <23412100+DrTexx@users.noreply.github.com>
2019-11-20 13:04:42 +1100

    [operator] now using logging module, formatted with black

--- volux/operator.py
+++ volux/operator.py
@@ -4,7 +4,21 @@ from .broker import VoluxBroker
 from .connection import VoluxConnection
 from threading import Thread
 import colorama
+
 colorama.init()
+import logging
+
+log = logging.getLogger("volux broker")
+log.setLevel(logging.DEBUG)
+# create console handler with a higher log level
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+# create formatter and add it to the handlers
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+ch.setFormatter(formatter)
+# add the handlers to the logger
+log.addHandler(ch)
+
 
 class VoluxOperator:
     def __init__(self):
@@ -16,29 +30,56 @@ class VoluxOperator:
         self.threads = []
         self.running = False
 
-    def add_module(self, module, req_permissions=[], check_module_repeats=True, overwrite_attributes=False):
+    def add_module(
+        self,
+        module,
+        req_permissions=[],
+        check_module_repeats=True,
+        overwrite_attributes=False,
+    ):
 
         if self.validate_module(module):  # if the object passed is a valid module
 
             if check_module_repeats == True:
 
-                if type(module) in [type(m) for m in self.modules.values()]:  # if this type of module is already in operator's modules
+                if type(module) in [
+                    type(m) for m in self.modules.values()
+                ]:  # if this type of module is already in operator's modules
 
-                    print("{}Warning: a different instance of the module '{}' is already loaded{}".format(colorama.Fore.YELLOW,module._module_name,colorama.Style.RESET_ALL))
+                    print(
+                        "{}Warning: a different instance of the module '{}' is already loaded{}".format(
+                            colorama.Fore.YELLOW,
+                            module._module_name,
+                            colorama.Style.RESET_ALL,
+                        )
+                    )
 
             if module.UUID in self.modules.keys():
 
-                raise Exception("this instance of module '{}' is already loaded! [{}{}{}]".format(module._module_name,colorama.Fore.GREEN,module.UUID,colorama.Style.RESET_ALL))
+                raise Exception(
+                    "this instance of module '{}' is already loaded! [{}{}{}]".format(
+                        module._module_name,
+                        colorama.Fore.GREEN,
+                        module.UUID,
+                        colorama.Style.RESET_ALL,
+                    )
+                )
 
             else:
 
-                setattr(module,'broker',self.broker)  # add broker to module
-                setattr(module,'req_permissions',req_permissions)  # add broker to module
+                setattr(module, "broker", self.broker)  # add broker to module
+                setattr(
+                    module, "req_permissions", req_permissions
+                )  # add broker to module
                 self.modules.update({module.UUID: module})  # add module to operator
                 self.permissions.update({module.UUID: req_permissions})
 
                 if module._module_attr in dir(self):  # if attrbute already present
-                    attribute_warning_string = "{}Warning: attribute '{}' is already being used by operator{}".format(colorama.Fore.YELLOW,module._module_name,colorama.Style.RESET_ALL)
+                    attribute_warning_string = "{}Warning: attribute '{}' is already being used by operator{}".format(
+                        colorama.Fore.YELLOW,
+                        module._module_name,
+                        colorama.Style.RESET_ALL,
+                    )
                     if overwrite_attributes == False:
                         print(attribute_warning_string + ", skipping...")
                     elif overwrite_attributes == True:
@@ -67,7 +108,7 @@ class VoluxOperator:
 
     def validate_module(self, module):
 
-        for attrib in ["_module_name", "_module_attr", 'get', 'set', 'UUID']:
+        for attrib in ["_module_name", "_module_attr", "get", "set", "UUID"]:
 
             if not hasattr(module, attrib):
 
@@ -84,7 +125,7 @@ class VoluxOperator:
 
         return self.modules
 
-    def add_connection(self,connection):
+    def add_connection(self, connection):
 
         if type(connection) == VoluxConnection:
 
@@ -99,7 +140,7 @@ class VoluxOperator:
 
             raise TypeError("connection must be an instance of VoluxConnection")
 
-    def remove_connection(self,connection):
+    def remove_connection(self, connection):
 
         if connection.UUID in self.connections:
 
@@ -107,7 +148,11 @@ class VoluxOperator:
 
         else:
 
-            raise ValueError("Can not remove connection: UUID is not in connections ({})".format(connection.UUID))
+            raise ValueError(
+                "Can not remove connection: UUID is not in connections ({})".format(
+                    connection.UUID
+                )
+            )
 
     def start_sync(self):
 
@@ -119,23 +164,24 @@ class VoluxOperator:
 
                 wrapped_sync = self._wrap_sync(connection.sync)
 
-                self.threads.append(
-                    Thread(target=wrapped_sync)
-                )
+                self.threads.append(Thread(target=wrapped_sync))
 
             self.running = True
 
             for thread in self.threads:
                 thread.start()
 
-            print("{}[CONNECTION SYNC STARTED]{}".format(colorama.Fore.YELLOW,colorama.Style.RESET_ALL))
+            print(
+                "{}[CONNECTION SYNC STARTED]{}".format(
+                    colorama.Fore.YELLOW, colorama.Style.RESET_ALL
+                )
+            )
 
         else:
 
             raise Exception("volux operator has no connections to start sync on!")
 
-    def _wrap_sync(self,sync_method):
-
+    def _wrap_sync(self, sync_method):
         def wrapped_sync():
 
             while self.running == True:
@@ -153,4 +199,8 @@ class VoluxOperator:
 
         self.threads = []
 
-        print("{}[CONNECTION SYNC STOPPED]{}".format(colorama.Fore.YELLOW,colorama.Style.RESET_ALL))
+        print(
+            "{}[CONNECTION SYNC STOPPED]{}".format(
+                colorama.Fore.YELLOW, colorama.Style.RESET_ALL
+            )
+        )
