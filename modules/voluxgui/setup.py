321b3b5d316890d73155b79b82490920aa0672c3
DrTexx <23412100+DrTexx@users.noreply.github.com>
2019-11-23 15:23:55 +1100

    [voluxgui] remove some metadata from setup

--- modules/voluxgui/setup.py
+++ modules/voluxgui/setup.py
@@ -6,39 +6,6 @@ from os import path
 
 __package_name__ = "voluxgui"
 
-classifiers = [
-    "Development Status :: 4 - Beta",
-    "Environment :: Console",
-    "Environment :: MacOS X",
-    "Environment :: Win32 (MS Windows)",
-    "Environment :: X11 Applications",
-    "Intended Audience :: Developers",
-    "Intended Audience :: End Users/Desktop",
-    "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-    "Natural Language :: English",
-    "Operating System :: MacOS",
-    "Operating System :: Microsoft :: Windows",
-    "Operating System :: POSIX :: Linux",
-    "Programming Language :: Python",
-    "Programming Language :: Python :: 3",
-    "Topic :: Games/Entertainment",
-    "Topic :: Home Automation",
-    "Topic :: Multimedia",
-    "Topic :: Scientific/Engineering :: Human Machine Interfaces",
-    "Topic :: Software Development",
-    "Topic :: Software Development :: Libraries",
-    "Topic :: Software Development :: User Interfaces",
-]
-
-project_urls = {  # a browsable URL for the project and a label for it, separated by a comma
-    "Bug Reports": "https://github.com/drtexx/{}/issues".format(
-        __package_name__
-    ),
-    "Source": "https://github.com/drtexx/{}".format(__package_name__),
-    "Funding": "https://paypal.me/denverpallis",
-    "Docs": "https://{}.readthedocs.io".format(__package_name__),
-}
-
 readme_filename = "README.md"
 readme_encoding = "utf-8"
 long_description_content_type = "text/markdown"
@@ -60,12 +27,10 @@ setuptools.setup(
     author="Denver Pallis",
     author_email="DenverPallisProjects@gmail.com",
     license="GPLv3+",
-    classifiers=classifiers,
     install_requires=["volux==0.9.16"],
     setup_requires=["wheel", "setuptools"],
     requires_python=[">=3"],
     requires_external=["python3-tk"],
-    project_urls=project_urls,
     entry_points={  # Optional
         "console_scripts": [
             "{}={}:__main__.main".format(__package_name__, __package_name__)
