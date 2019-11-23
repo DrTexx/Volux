00d109647e43d4e612379e322f8498d1c99a3540
DrTexx <23412100+DrTexx@users.noreply.github.com>
2019-11-23 11:19:43 +1100

    updated guivis demo

--- volux/demos/guivis.py
+++ volux/demos/guivis.py
@@ -14,20 +14,16 @@ class DemoGuiVis(VoluxDemo):
     def run_demo(self):
 
         import volux  # import essentials
-        from voluxaudio import (
-            VoluxAudio,
-        )  # module to get amplitude of desktop audio
-        from volux.modules import (
-            VoluxCliPrint,
-        )  # module to print values to CLI
-        from voluxgui import VoluxGui  # module to generate GUI
+        import voluxaudio  # module to get amplitude of desktop audio
+        import voluxgui  # module to generate GUI
 
-        vlx = (
-            volux.VoluxOperator()
-        )  # instantiate an operator for managing connections
-        vlx.add_module(VoluxAudio())
-        vlx.add_module(VoluxCliPrint())
-        vlx.add_module(VoluxGui())
+        # instantiate an operator for managing connections
+        vlx = volux.VoluxOperator()
+        # add modules
+        vlx.add_module(volux.modules.VoluxCliPrint())
+        vlx.add_module(voluxaudio.VoluxAudio())
+        vlx.add_module(voluxgui.VoluxGui())
+        # add connections
         vlx.add_connection(
             volux.VoluxConnection(vlx.audio, vlx.gui, 120)
         )  # change GUI bar to match audio amplitude
