a937855521f951319d2ccb002950de98686dd24f
DrTexx <23412100+DrTexx@users.noreply.github.com>
2020-08-15 15:32:18 +1000

    update SolidFrameEngine and NoiseFrameEngine

--- volux/demos/MOVE_ME_tile/engines.py
+++ volux/demos/MOVE_ME_tile/engines.py
@@ -1,54 +1,85 @@
 from random import randint
 from math import ceil, floor
 
-from volux.demos.MOVE_ME_tile.parameters import (
+from .types import (
     HueParameters,
     SaturationParameters,
     BrightnessParameters,
     KelvinParameters,
 )
-from volux.demos.MOVE_ME_tile.frame.render_method import (
-    FrameRenderMethod,
-    FrameRenderMethodSettings,
+from .engine.frame import (
+    FrameEngine,
+    RenderingEngineSettings,
 )
+from .engine.motion import MotionEngine
 from .types import HSBK
 from .util import clamp
 
 
-class SolidFrameEngine(FrameRenderMethod):
+class SolidFrameEngine(FrameEngine):
     """Renders a frame where every pixel is the same color."""
 
     def __init__(self):
-        settings = FrameRenderMethodSettings(
+        settings = RenderingEngineSettings(
             hue=HueParameters(),
             saturation=SaturationParameters(),
             brightness=BrightnessParameters(),
             kelvin=KelvinParameters(),
         )
-        super().__init__(settings=settings, func=self._func)
+        super().__init__(settings=settings, render_func=self._func)
 
-    def _func(self, val):
+    def _func(self, val, settings):
+        print("settings not respected yet.")
         return [(0, 65535, (65535 / 100) * val, 3500) for _ in range(64)]
+        # for idx, pixel in enumerate(self.frame):
+        #     if idx % 2 == 0:
+        #         self.frame[idx] = (
+        #             (65535 / 1),
+        #             65535,
+        #             (65535 / 100) * (val / 100),
+        #             6500,
+        #         )
+        #     else:
+        #         if randint(0, 1) == 0:
+        #             self.frame[idx] = (
+        #                 (65535 / 2),
+        #                 65535,
+        #                 (65535 / 50) * (val / 2),
+        #                 6500,
+        #             )
+        #         else:
+        #             self.frame[idx] = (
+        #                 (65535 / 3),
+        #                 65535,
+        #                 (65535 / 50) * (val / 2),
+        #                 6500,
+        #             )
+
+        return self.frame
 
 
-class NoiseFrameEngine(FrameRenderMethod):
+class NoiseFrameEngine(FrameEngine):
     def __init__(self):
-        settings = FrameRenderMethodSettings(
+        settings = RenderingEngineSettings(
             hue=HueParameters(),
             saturation=SaturationParameters(),
             brightness=BrightnessParameters(),
             kelvin=KelvinParameters(),
         )
-        super().__init__(settings=settings, func=self._func)
+        super().__init__(settings=settings, render_func=self._func)
 
-    def _func(self, val):
+    def _func(self, val, settings):
+        print("settings not respected yet.")
 
         max_probability = 10
         """(1 / this) is probability."""
         # make reactive pixels less likely when value is higher
         probability_of_reactive_pixel = int(max_probability * (val / 100))
         # make reactive more likely with more amplitude
-        # probability_of_reactive_pixel = int(5 * (100 / clamp(1, val, 100)))
+        # probability_of_reactive_pixel = int(
+        #     max_probability * (100 / clamp(1, val, 100))
+        # )
+        print(f"reactivity probability: 1 / {probability_of_reactive_pixel}")
 
         tile_colors = []
 
@@ -60,16 +91,18 @@ class NoiseFrameEngine(FrameRenderMethod):
                         (65535 / 1),
                         (65535 / 100) * (randint(80, 100)),
                         (65535 / 100) * val,
-                        3500,
+                        6500,
                     )
                 )
             else:
                 tile_colors.append(
                     HSBK(
                         (65535 / 2),
-                        (65535 / 100) * (randint(1, 100)),
-                        (65535 / 100) * val / 4,
-                        3500,
+                        (65535 / 100) * (randint(90, 100)),
+                        # (65535 / 100) * val / 10,
+                        # (65535 / 100) * (19 + (31 / max(1, val))),
+                        (65535 / 100) * (1 + (19 / max(1, val))),
+                        6500,
                     )
                 )
 
