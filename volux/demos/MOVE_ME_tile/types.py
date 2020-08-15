2d52defe2b0aba823ea4d659ed31cc75be770a1e
DrTexx <23412100+DrTexx@users.noreply.github.com>
2020-08-15 15:06:09 +1000

    WIP - move parameters into types.py

--- volux/demos/MOVE_ME_tile/types.py
+++ volux/demos/MOVE_ME_tile/types.py
@@ -11,5 +11,25 @@ class HSBK(NamedTuple):
 LifxTileFrame = List[HSBK]
 
 
-class FrameRenderFunction:
-    Callable[[int], LifxTileFrame]
+class HueParameters:
+    def __init__(self, _min=0, _max=65535):
+        self._min = _min
+        self._max = _max
+
+
+class SaturationParameters:
+    def __init__(self, _min=0, _max=65535):
+        self._min = _min
+        self._max = _max
+
+
+class BrightnessParameters:
+    def __init__(self, _min=0, _max=65535):
+        self._min = _min
+        self._max = _max
+
+
+class KelvinParameters:
+    def __init__(self, _min=0, _max=65535):
+        self._min = _min
+        self._max = _max
